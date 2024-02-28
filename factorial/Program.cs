77c3bdaab44e7ae422a74ec92d238022f41c18dc
jacoder <mantle@vivaldi.net>
2024-02-28 08:59:37 -0500

    FACTORIAL
    Imported the arbitrary-precision artihmetic namespace
    using System.Numerics
    To be able to use the BigInteger datatype to ensure the factorial
    program is capable of handling the results of the factorial of large
    numbers.

--- factorial/Program.cs
+++ factorial/Program.cs
@@ -5,67 +5,83 @@
     @datetime:: February 28, 2024 2:56 am (UTC-5)
     @author:: jac0der
 */
-
+// importing arbitrary-precision arithmetic datatypes namespace.
+using System.Numerics;
 
 /***
-    Function to get input number from user..
-    @output:: number successfully cast from string input.
+    Function to get input number from user. Prompt user for an input
+    until a valid input is provided.
+    @input::    none
+    @output::   number successfully cast from string input to integer.
 ***/
-long get_input()
+int get_input()
 {
-    long number;
+    int number;
     do
     {
         try
         {
             Console.Write("Enter number: ");
-            // accept number from user            
-            number = Convert.ToInt64(Console.ReadLine());
 
-            if(number < 0)
+            // accept number from user            
+            number = Convert.ToInt32(Console.ReadLine());
+
+            // check for negatives and ignore
+            if(number <= 0)
             {
-                Console.WriteLine("Please provide a positive integer");
+                Console.WriteLine("Please provide a positive integer.");
             }
             else
             {
+                // return valid positive number
                 return number;
             }
         }
         catch(FormatException fe)
         {
-            Console.WriteLine("Please provide a numeric input");
+            Console.WriteLine("Please provide a numeric input.");
             Console.WriteLine(fe.Message);
         }       
     }
-    while(true);   
+    while(true);
 }
 
 /***
     Function to find the factorial of a number
-    @input:: number to retrieve factorial for
-    @output:: factorial of input number
+    @input::int 
+        number to retrieve factorial for.
+
+    @output::BigInteger   
+        factorial of input number uisng BigInteger
+        Arbitrary-precision arithmetic datattype,
+        to handle factorial results of large numbers.
 ***/
-long factorial(long number)
+BigInteger factorial(int number)
 {     
     // 1! is 1 so return 1 - base case met
-    if(number == 1 || number == 0)
+    if(number == 1)
         return 1;
 
-    // recursively call factorial on n - 1
-    return number * factorial(number - 1);
+    // recursively call factorial on number - 1
+    BigInteger fact = number * factorial(number - 1);
+
+    return fact;
 }
 
 /***
     function to perform the factorial of number
     if entered number is valid.
+    @input:: none
+    @output:: none (void function)
 ***/
 void calculate_factorial()
 {
-    long user_input = get_input();
+    int user_input = get_input();
 
     if(user_input >= 0)
     {
-        Console.WriteLine($"Factorial of {user_input} is: " + factorial(user_input));
+        // print factorial result in scientific notation with e+15 format
+        Console.WriteLine($"Factorial of {user_input} is: " + factorial(user_input).ToString("e15"));
     }
 }
 
