36b065e9a9dfd96f4ee1db562ecb45079dbe0d93
jacoder <mantle@vivaldi.net>
2024-03-03 13:11:53 -0500

    VARIABLES
    Created project for exporing c# variables.
    
    Also created a separate folder for recursive programs.

---
+++ variables/Models/Person.cs
@@ -0,0 +1,9 @@
+namespace variables.Models
+{
+    public class Person
+    {
+        public string first_name { get; set; } = string.Empty;
+        public string last_name { get; set; } = string.Empty;
+    }
+}
+
---
+++ variables/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+    Variables are simple place holders for data used within a 
+    computer program. Specifically, a variable tells the 
+    computer to reserve some location in memory to store 
+    data of a certain type. The name of the variable is 
+    then used to refer to this memory location where the 
+    value is stored.
+
+    C# null coalescing operator.
+    Declaring variables describing a favorite song.
+    
+    @Modified Datetime:: March 03, 2024 12:26 pm (UTC-5)
+    @author:: jac0der
+*/
+using variables.Models;
+
+// creating a nullable type with the ? (question mark)
+int? year = null;
+
+Person? person = null;
+
+/*
+    The C# double question mark operator ?? is known as the null 
+    coalescing operator.
+    In a nutshell, the null coalescing operator is "if the first 
+    value is null, use this other value."
+    Null coalescing operator is a binary operator that takes two 
+    operands: the first operand is an expression that can be null, 
+    and the second operand is the value that I want to assign to 
+    the expression variable (personValue) if it is not null.
+*/
+var personValue = person ?? new Person()
+{
+    first_name = "Jim",
+    last_name = "bo"
+};
+
+Console.WriteLine(personValue.first_name);
