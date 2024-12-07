18c4ec460914ee86730d0d2e41dc56e31297abfe
jacoder <mantle@vivaldi.net>
2024-12-07 13:09:58 -0500

    STRINGS MANIPULATION
    used string interpolation feature.

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -21,7 +21,30 @@ Console.WriteLine("uppercased: " + name.ToUpper());
 Console.WriteLine("lowercased: " + name.ToLower());
 
 
-//** 4 => string concatenation
+//** 4 => string concatenation (+)
 string firstname = "Jane", lastname = "Doe";  // declaring multiple variables in one line, different values.
 string fullname = firstname + " " + lastname;
 Console.WriteLine("fullname is: " + fullname);
+// using the string.Concat() method
+Console.WriteLine(string.Concat(firstname," ", lastname, " How are you", "?"));
+
+
+//** 5 => string interpolation (another form of concatenation)
+string food = "pizza";
+string person = "John";
+string message = $"{person}'s favorite food is {food}.";
+Console.WriteLine(message);
+
+
+//** 6 => accessing characters within a string
+string country = "Jamaica";
+Console.WriteLine(country[2]);  // m
+
+
+
+
+
+
+
+
+
